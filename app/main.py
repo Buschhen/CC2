4139294578e1171ad778ee76e1c19fe839861e4c
Busch, Henrik <Henrik.Busch@adidas.com>
2025-04-03 18:19:19 +0200

    added hostname

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    hostname = socket.gethostname()
+    return f'Hello, World! This is VM: {hostname}'
 
 if __name__ == '__main__':
     print('testing')
