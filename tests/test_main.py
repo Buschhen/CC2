9ed911212f7660d3760b3b39466209cec573e17d
Henrik Busch <henrik.busch@gmx.de>
2025-04-07 03:46:28 +0200

    test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,12 @@
 from unittest.mock import patch, MagicMock
-from app.main import app
 
-@patch("app.main.container_client")
-def test_index_route(mock_container_client):
-    mock_container_client.list_blobs.return_value = []
+@patch("app.main.BlobServiceClient")
+def test_index_route(mock_blob_service):
+    mock_blob_instance = MagicMock()
+    mock_blob_instance.get_container_client.return_value.list_blobs.return_value = []
+    mock_blob_service.from_connection_string.return_value = mock_blob_instance
 
+    from app.main import app  # Jetzt importieren, nachdem BlobServiceClient gepatcht ist
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
