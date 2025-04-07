697ff6fc118f4baa9aab80dac0e4f350f2964c7c
Henrik Busch <henrik.busch@gmx.de>
2025-04-07 03:44:45 +0200

    test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,10 @@
+from unittest.mock import patch, MagicMock
 from app.main import app
 
-def test_index_route():
+@patch("app.main.container_client")
+def test_index_route(mock_container_client):
+    mock_container_client.list_blobs.return_value = []
+
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
